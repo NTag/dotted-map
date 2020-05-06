82bb225c5b606b32d3cbf7dd51fae796561f05b7
Basile Bruneau <basilebruneau@gmail.com>
2020-05-06 21:37:02 +0200

    fix: ignore test.js

--- test.js
+++
@@ -1,9 +0,0 @@
-const DottedMap = require('./index.js');
-
-const map = new DottedMap({ height: 100, grid: 'diagonal' });
-
-const pinColor = '#D6FF79';
-
-map.addPin({ lat: 48.85, lng: 2.35, svgOptions: { color: pinColor, radius: 0.22 } });
-
-console.log(map.getSVG({ radius: 0.22, color: '#423B38', shape: 'circle', backgroundColor: '020300' }));
